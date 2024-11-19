fba6505609ff744283d556a859c66ab759626dc1
Lucy <lucy@nix2twink.gay>
2024-11-19 07:49:41 +0100

    bvg support

--- src/SearchStations.tsx
+++ src/SearchStations.tsx
@@ -1,28 +1,42 @@
 import React, {useState, useEffect} from "react";
 import Station from "./Station";
 import axios from "axios";
-import {styled, TextField} from "@mui/material";
 import StyledTextField from "./components/StyledTextField";
-
-
+import {createClient} from 'hafas-client'
+import {profile} from 'hafas-client/p/db/index'
 const SearchStations: React.FC = () => {
     const [inputValue, setInputValue] = useState<string>("");
     const [stations, setStations] = useState<Station[]>([]);
+    const [departures, setDepartures] = useState<any[]>([]); // State for departures
+    const [selectedStations, setSelectedStations] = useState<Station[]>([]);
+
 
     const fetchStations = async (query: string) => {
         if (query) {
             try {
-                const response = await axios.get(`https://v6.vbb.transport.rest/stations?query=${query}`);
+                const response = await axios.get(`https://v6.db.transport.rest/stations?query=${query}`);
                 const stationsData = response.data;
 
-
                 // Convert the object into an array of stations
-                const stationsArray: Station[] = Object.values(stationsData).map((station: any) => ({
+                let stationsArray: Station[] = Object.values(stationsData).map((station: any) => ({
                     id: station.id,
                     name: station.name,
                     latitude: station.location.latitude,
                     longitude: station.location.longitude,
                 }));
+
+                if (stationsArray.length == 0) {
+                    const response = await axios.get(`https://v6.vbb.transport.rest/stations?query=${query}`);
+                    const stationData = response.data;
+
+                    stationsArray = Object.values(stationData).map((station: any) => ({
+                        id: station.id,
+                        name: station.name,
+                        latitude: station.location.latitude,
+                        longitude: station.location.longitude,
+                    }));
+                }
+
                 setStations(stationsArray);
             } catch (error) {
                 console.error("Error fetching stations:", error);
@@ -61,16 +75,16 @@ const SearchStations: React.FC = () => {
                         value={inputValue}
                         onChange={handleChange}
                         fullWidth
-                        />
+                    />
                     <i className="fas fa-sliders-h"></i>
                 </header>
 
 
             </div>
-            <ul>
+            <ul style={{ background: 'inherit', color: 'inherit'}}>
                 {stations.map((station) => (
                     <li key={station.id}>
-                        {station.name} (Lat: {station.latitude}, Lon: {station.longitude})
+                        {station.name}
                     </li>
                 ))}
             </ul>
